5b0c3a87860e39ca64140ffff99aabf236a6784a
Swarup Ghosh <codecrafts.cf@icloud.com>
2020-04-14 01:15:55 +0530

    Removed @tf.function for as_float due to tf retraceability errors

--- ml_tour/helpers.py
+++ ml_tour/helpers.py
@@ -11,7 +11,7 @@ def check_1d(tensor: tf.Tensor, name: str):
 def check_2d(tensor: tf.Tensor, name: str):
     assert len(tf.shape(tensor)) == 2, "%s must be represented as a 2D array only" % name
 
-@tf.function
+@typechecked
 def as_float(tensor: Union[tf.Tensor, np.ndarray]) -> tf.Tensor:
     tensor = tf.convert_to_tensor(tensor)
     tensor = tf.cast(tensor, tf.float32)
