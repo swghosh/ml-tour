9dc305f1a72c2426e505673f530d416ceaf63143
Swarup Ghosh <codecrafts.cf@icloud.com>
2020-04-13 23:38:41 +0530

    Added get_mean_stdev, normalise to helpers

--- ml_tour/helpers.py
+++ ml_tour/helpers.py
@@ -1,4 +1,4 @@
-import tensorboard as tf
+import tensorflow as tf
 import numpy as np
 from typeguard import typechecked
 from typing import Union
@@ -7,8 +7,18 @@ from typing import Union
 def check_2d(tensor: tf.Tensor, name: str):
     assert len(tf.shape(tensor)) == 2, "%s must be represented as a 2D array only" % name
 
-@typechecked
+@tf.function
 def as_float(tensor: Union[tf.Tensor, np.ndarray]) -> tf.Tensor:
     tensor = tf.convert_to_tensor(tensor)
     tensor = tf.cast(tensor, tf.float32)
     return tensor
+
+@tf.function
+def get_mean_stdev(X: tf.Tensor) -> (tf.Tensor, tf.Tensor):
+    mean = tf.reduce_mean(X, axis=0)
+    stdev = tf.reduce_std(X, axis=0)
+    return mean, stdev
+
+@tf.function
+def normalise(X: tf.Tensor, mean: tf.Tensor, stdev: tf.Tensor) -> tf.Tensor:
+    return (X - mean) / stdev
